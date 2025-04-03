bf1572214d64e504cc588500675cfd98652fc5ee
luis-otavio-dias <luis777dias231@gmail.com>
2025-04-03 16:47:14 -0300

    Criando models Tag e Category, slug e slugify

---
+++ djangoapp/utils/rands.py
@@ -0,0 +1,16 @@
+import string
+from random import SystemRandom
+from django.utils.text import slugify
+
+
+def random_letters(k=5):
+    return "".join(
+        SystemRandom().choices(
+            string.ascci_lowercase + string.digits,
+            k=k,
+        )
+    )
+
+
+def slugify_new(text, k=5):
+    return slugify(text) + "-" + random_letters(k)
