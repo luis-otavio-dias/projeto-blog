9ad435d44c282743256961cc83b9bdc1a2318dda
luis-otavio-dias <luis777dias231@gmail.com>
2025-04-24 08:48:24 -0300

    Configurando created_by e updated_by em ModelAdmin save_model

--- djangoapp/blog/admin.py
+++ djangoapp/blog/admin.py
@@ -82,3 +82,11 @@ class PostAdmin(admin.ModelAdmin):
         "tags",
         "category",
     )
+
+    def save_model(self, request, obj, form, change):
+        if change:
+            obj.updated_by = request.user
+        else:
+            obj.created_by = request.user
+
+        obj.save()
